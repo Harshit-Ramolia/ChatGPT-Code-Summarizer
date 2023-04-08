a218775b08d5aa1bcb5e6e569433a99ffe802ce7
Harshit Ramolia <ramolia.mh@iitgn.ac.in>
2023-04-08 21:52:24 +0530

    reorganis

---
+++ file_tree.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass, field
+from extension_list import isExtension
+from scrapper import Scrapper
+from typing import List
+from utility import *
+
+
+@dataclass
+class Node:
+    name: str = None
+    root: str = None
+    childrens: dict[str, "Node"] = field(default_factory=dict)
+    files: List[str] = field(default_factory=list)
+    no_files: bool = True
+    full_summary: str = ""
+    files_summary: List[str] = field(default_factory=list)
+
+
+@dataclass
+class Tree:
+    startpath: str = ".\code"
+    root: Node = field(default=None, init=None)
+    semi: bool = False
+
+    def __make_node(self, root: str, name: str, dirs: List[str], files: List[str]) -> Node:
+        files = list(filter(lambda x: isExtension(x.split(".")[1]), files))
+        files = list(filter(lambda x: not is_gitignored(root+"\\"+x), files))
+        new_node = Node(name=name, root=root, files=files)
+        for folders in dirs:
+            new_node.childrens[folders] = None
+        return new_node
+
+    def __fill_no_files(self, node: Node) -> bool:
+        do_child_have_files = False
+        for name in node.childrens:
+            val = self.__fill_no_files(node.childrens[name])
+            do_child_have_files = do_child_have_files or val
+        do_current_have_files = len(node.files) > 0
+        have_files = do_child_have_files or do_current_have_files
+        node.no_files = not have_files
+        return have_files
+
+    def __remove_no_files(self, node: Node) -> Node:
+        if (node.no_files == True):
+            return None
+
+        remove = []
+        for name in node.childrens:
+            new_node = self.__remove_no_files(node.childrens[name])
+            if (new_node != None):
+                node.childrens[name] = new_node
+            else:
+                remove += [name]
+
+        for name in remove:
+            del node.childrens[name]
+
+        return node
+
+    def __add(self, root: str, dirs: List[str], files: List[str]) -> None:
+        current_node = self.root
+        path = root[1:].split('\\')[1:]
+        if (current_node == None):
+            new_node = self.__make_node(
+                root=root, name=path[0], dirs=dirs, files=files)
+            self.root = new_node
+            return
+        for name in path[1:]:
+            if (current_node.childrens[name] == None):
+                new_node = self.__make_node(
+                    root=root, name=name, dirs=dirs, files=files)
+                current_node.childrens[name] = new_node
+                return
+            current_node = current_node.childrens[name]
+
+    def fill(self) -> None:
+        startpath = self.startpath
+        assert os.path.exists(
+            startpath), f"Directory {startpath} doesn't exist"
+        for ele in os.walk(startpath):
+            self.__add(*ele)
+
+    def sanitize(self) -> None:
+        self.__fill_no_files(self.root)
+        self.root = self.__remove_no_files(self.root)
+
+    def __summarize(self, node: Node, driver: Scrapper, prompt: str) -> None:
+        children_summaries = []
+        for name in node.childrens:
+            response = self.__summarize(node.childrens[name], driver)
+            response = f"The following is the documentaion and summary for directory {name}\n\n {response} \n\n"
+            children_summaries += [response]
+            print(f"completed dir = {node.root}\\{name}")
+
+        files_summary = []
+        for file in node.files:
+            input = file_content(node.root+"\\"+file)
+            input += f"\n\n{prompt}\n\n"
+            response = driver.chatGPT(input)
+            response = f"The following is the documentaion and summary for file {file}\n\n {response} \n\n"
+            files_summary += [response]
+            print(f"completed file = {node.root}\\{file}")
+
+        node.files_summary = files_summary
+
+        input = f"\n\n{prompt}\n\n"
+        summary = "\n".join(children_summaries+files_summary)
+
+        response = driver.chatGPT(summary+input)
+        if (not self.semi):
+            file_create(f"{node.root}\document.md", response)
+        node.full_summary = response
+        return response
+
+    def fill_summaries(self, driver: Scrapper, prompt: str = "Can you please summarize the above content in markdown format also use tables wherever feels good. Please explain what the code does, what parameters it takes and what it returns. Keep it simple. Please also ensure that the response is provided in markdown format so that I can easily copy and paste it to a file or document. Thank you.") -> None:
+        self.__summarize(self.root, driver, prompt)
+        if (self.semi):
+            file_create(f"{self.root.root}\document.md", self.root.full_summary)
--- index.py
+++ index.py
@@ -1,8 +1,32 @@
-from utility import Tree
-import os
+import argparse
+from file_tree import Tree
+from scrapper import Scrapper
 
-FileStruct = Tree()
-startpath = ".\code"
-for ele in os.walk(startpath):
-    FileStruct.add(*ele)
-FileStruct.sanitize()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="ChatGPT Code Summarizer",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--semi", action="store_true",
+                        help="Create files for main directory only")
+    parser.add_argument("--path", help="Location of code (default .\\code)")
+    parser.add_argument("--prompt", help="Custom prompt for ChatGPT")
+    args = parser.parse_args()
+    config = vars(args)
+
+    driver = Scrapper()
+    driver.start()
+    driver.chatGPT(
+        "Hello, can you help me summaries all the codes and file I give you?")
+
+    if (config["path"]):
+        FS = Tree(startpath=config["path"], semi=config["semi"])
+    else:
+        FS = Tree(semi=config["semi"])
+
+    FS.fill()
+    FS.sanitize()
+    if (config["prompt"]):
+        FS.fill_summaries(driver=driver, prompt=config["prompt"])
+    else:
+        FS.fill_summaries(driver=driver)
